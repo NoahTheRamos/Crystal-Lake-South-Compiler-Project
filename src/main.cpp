5d6ec880d9f952e4ddb67c89d9c2fa9b67dd73c6
NoahTheRamos <143461775+NoahTheRamos@users.noreply.github.com>
2023-11-01 22:48:54 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
-#define EXIT_SUCCESS 0
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Incorrect usage!" << std::endl;
+        std::cerr << "ncc <input.ns>" << std::endl;
+        return EXIT_FAILURE;
+    }
 
+    std::string contents;
+    {
+        std::stringstream contents_stream;
+        std::fstream input(argv[1], std::ios::in);
+        contents_stream << input.rdbuf();
+        contents = contents_stream.str();
+    }
+    
+    std::cout << argv[1] << std::endl;
+    std::cout << contents << std::endl;
+    
     return EXIT_SUCCESS;
 }
